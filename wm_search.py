a305c6b6599e6004ac9dabdc9e46096234c7f48d
shariqiqbal2810 <shariqiqbal2810@users.noreply.github.com>
2016-02-24 16:14:58 -0500

    Adjusted flicker size to match photodiode

--- wm_search.py
+++ wm_search.py
@@ -165,10 +165,10 @@ class Stimuli:
         Send a binary signal (value) to the photodiode by flickering a white
         circle in the bottom right hand corner.
         """
-        circle = visual.Circle(self.win, units='height', radius=0.1,
+        circle = visual.Circle(self.win, units='height', radius=0.05,
                                fillColorSpace='rgb255',
                                lineColorSpace='rgb255',
-                               fillColor=(0, 0, 0), pos=(0.78, -0.4),
+                               fillColor=(0, 0, 0), pos=(0.75, -0.45),
                                lineColor=(0, 0, 0))
         value = np.binary_repr(value)
         # zero pad to 8 bits and add stop and start bits
